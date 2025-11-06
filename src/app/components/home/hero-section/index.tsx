46fc1d00ddf6b9e187b69f7792ddb5dd81c00a2e
mohamed lamine gaci <mohamed61lamine@gmail.com>
2025-11-06 16:36:35 +0100

    first commit

---
+++ src/app/components/home/hero-section/index.tsx
@@ -0,0 +1,54 @@
+import { getImgPath } from "@/utils/image";
+import Image from "next/image";
+
+const index = () => {
+  return (
+    <section className="relative hero-section overflow-hidden pt-35 md:pt-40 pb-12 lg:pb-30 xl:pt-52">
+      <div className="container">
+        <div className="lg:flex grid grid-cols-1 sm:grid-cols-2 gap-7 md:gap-4 items-center">
+          <div className="flex flex-col gap-4 md:gap-7 max-w-2xl">
+            <div>
+              <div className="flex items-center gap-8">
+                <h3>I'm Mohamed Lamine</h3>
+                <div className="wave">
+                  <Image
+                    src={getImgPath("/images/home/banner/wave-icon.svg")}
+                    alt="wave-icon"
+                    width={62}
+                    height={62}
+                    className=""
+                  />
+                </div>
+              </div>
+              <h3>Full-Stack Developer | PHP Laravel Specialist</h3>
+            </div>
+            <p className="text-secondary font-normal max-w-md xl:max-w-xl">
+              Young dynamic and motivated web developer, passionate about computer science and
+              new technologies. I specialize in creating robust and scalable web applications using PHP and the Laravel framework. 
+              With a keen eye for detail and a commitment to delivering high-quality code, I strive to build solutions that not only meet but exceed client expectations.
+
+            </p>
+          </div>
+          <Image
+            src={getImgPath("/images/home/banner/banner-img.png")}
+            alt="banner-img"
+            width={685}
+            height={650}
+            className="block lg:hidden"
+          />
+        </div>
+      </div>
+      <div className="absolute right-0 top-0 hidden h-auto w-1/2 lg:block 2xl:h-171.5 2xl:w-187.5">
+        <Image
+          src={getImgPath("/images/home/banner/banner-img.png")}
+          alt="banner-img"
+          width={685}
+          height={650}
+          className=" absolute right-0 top-0 z-1"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default index;
