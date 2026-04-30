440a72b07dd44e3571d9bb7a7a2f9893cecab59c
mohamed lamine <mohamed61lamine@gmail.com>
2026-05-01 00:17:43 +0100

    fix: update output configuration in next.config.ts to use environment variable

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: process.env.NEXT_OUTPUT === "export" ? "export" : undefined,
   eslint: {
     ignoreDuringBuilds: true, // ✅ Skip ESLint checks on production build
   },
