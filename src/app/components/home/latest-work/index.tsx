a621d2aa439eea81f635f382a6c8e9f94c61de1f
mohamed lamine <mohamed61lamine@gmail.com>
2026-07-16 00:23:19 +0100

    ajouter de nouveaux projets

--- src/app/components/home/latest-work/index.tsx
+++ src/app/components/home/latest-work/index.tsx
@@ -146,7 +146,7 @@ const LatestWork = () => {
           ) : workData ? (
             <>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {workData.slice(0, 6).map((project, index) => (
+                {workData.slice(0, 9).map((project, index) => (
                   <ProjectCard key={project.slug} project={project} index={index} />
                 ))}
               </div>
